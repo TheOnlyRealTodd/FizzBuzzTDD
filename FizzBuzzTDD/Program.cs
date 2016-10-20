8e8e5eb7eb9d1a9a3828a0d8b512c23d23b7ced1
Todd <toddcullum@gmail.com>
2016-10-20 03:44:59 -0700

    First commit

---
+++ FizzBuzzTDD/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzzClasses;
+
+namespace FizzBuzzTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(FizzBuzzer.GetValue(i));
+            }
+        }
+    }
+    
+}
